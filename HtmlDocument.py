241d917547f3ca8e8897a91eb07954b9368838d5
Mohammad Reza mansouri <beekalam@gmail.com>
2021-02-02 18:46:57 +0330

    [REFACTOR] HtmlDocument

--- HtmlDocument.py
+++ HtmlDocument.py
@@ -7,9 +7,8 @@ class HtmlDocument:
     def __init__(self, styles="") -> None:
         self.jsContent = []
         self.cssContent = []
-        # self.highlightjs_js = read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "highlight.min.js"))
         self.addJS(read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "highlight.min.js")))
-        self.highlightjs_css = read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "default.min.css"))
+        self.addCSS(read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "default.min.css")))
         self.styles = styles
 
     def addJS(self, js):
@@ -19,25 +18,22 @@ class HtmlDocument:
         self.cssContent.append(style)
 
     def make(self, content):
-        hljs = """
-            <style>{}</style>
-            """.format(self.highlightjs_css)
+        hljs = ""
 
-
-        for css in self.cssContent:
-            hljs += "<style>{}</style>".format(css)
-
-        for js in self.jsContent:
-            hljs += "<script>{}</script>".format(js)
-
-        hljs = hljs + """
+        self.addJS("""
             <script>
                 document.querySelectorAll('pre.highlight').forEach(block => {
                   // then highlight each
                   hljs.highlightBlock(block);
                 });
             </script>
-        """
+        """)
+
+        for css in self.cssContent:
+            hljs += "<style>{}</style>".format(css)
+
+        for js in self.jsContent:
+            hljs += "<script>{}</script>".format(js)
 
         return """
         <html>
