fa899e3cd65e212693f71bce73353254e70b7a74
Mohammad Reza mansouri <beekalam@gmail.com>
2021-02-02 19:38:18 +0330

    [REFACTOR]  HtmlDocument removed hightlight js specific.

--- HtmlDocument.py
+++ HtmlDocument.py
@@ -5,33 +5,16 @@ from utils import read_file_content, HIGHLIGHT_JS_PATH
 
 class HtmlDocument:
     def __init__(self) -> None:
-        self.jsContent = []
-        self.cssContent = []
-        self.addCSS(read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "default.min.css")))
-        self.addJS(read_file_content(os.path.join(HIGHLIGHT_JS_PATH, "highlight.min.js")))
+        self.jsContent = ""
+        self.cssContent = ""
 
     def addJS(self, js):
-        self.jsContent.append(js)
+        self.jsContent = self.jsContent + "<script>{}</script>".format(js)
 
     def addCSS(self, style):
-        self.cssContent.append(style)
+        self.cssContent = self.jsContent + "<style>{}</style>".format(style)
 
     def make(self, content):
-
-        self.addJS("""
-                document.querySelectorAll('pre.highlight').forEach(block => {
-                  hljs.highlightBlock(block);
-                });
-        """)
-
-        css_content = ""
-        for css in self.cssContent:
-            css_content = css_content + "<style>{}</style>".format(css)
-
-        js_content = ""
-        for js in self.jsContent:
-            js_content = js_content + "<script>{}</script>".format(js)
-
         html = """
         <html>
         <head>
@@ -42,5 +25,5 @@ class HtmlDocument:
             {}
         </body>
         </html>
-        """.format(css_content, content, js_content)
+        """.format(self.cssContent, content, self.jsContent)
         return html
