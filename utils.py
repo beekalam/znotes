3ee4a7d2ecf3b313b9f531f7004e98b4db34b03d
Mohammad Reza mansouri <beekalam@gmail.com>
2021-02-04 08:08:29 +0330

    [DOC] utils.py function docs.

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ HIGHLIGHT_JS_PATH = os.path.join(os.path.dirname(__file__), "highlight.js")
 
 
 def read_tags(path):
+    """Reads tags from files"""
     lines = open(path, 'r').read().split("\n")
     res = list(filter(lambda x: x.startswith("tags"), lines))
     if res:
@@ -13,6 +14,7 @@ def read_tags(path):
 
 
 def build_tags_list():
+    """Aggregates tags from a list of files."""
     tags = []
     for file in build_file_list():
         tags.extend(read_tags(file))
@@ -21,6 +23,7 @@ def build_tags_list():
 
 
 def build_file_list():
+    """Build list of notes"""
     files_list = []
     for root, dirs, files in os.walk(NOTES_PATH):
         for file in files:
@@ -30,11 +33,6 @@ def build_file_list():
     return files_list
 
 
-# print(build_tags_list())
-# print(build_file_list())
-# print(list(enumerate(build_tags_list())))
-# for i, path in enumerate(build_tags_list()):
-#     print(i, path)
 def search_file_content_old(path, query):
     with open(path) as f:
         content = f.read()
@@ -54,17 +52,13 @@ def search_file_content(path, queries):
 
 
 def search(query: str):
-    # terms = query.split(":")
+    """Search in files"""
     terms = [q for q in query.split(':') if not q.startswith('#')]
-    # search_tags = terms[0].replace('#', '')
     search_tags = [q.replace('#', '') for q in query.split(':') if q.startswith('#')]
 
-    # print(tag)
-    # print(terms[1:])
     res = []
     for file in build_file_list():
         file_tags = read_tags(file)
-        # if search_tags in file_tags:
         if len(search_tags) == 0 or any([search_tag in file_tags for search_tag in search_tags]):
             if len(terms):
                 s = search_file_content(file, terms)
@@ -76,16 +70,20 @@ def search(query: str):
 
 
 def read_file_content(path):
+    """Reads content of file into string"""
     with open(path) as f:
         return f.read()
 
 
 def file_put_content(path, content):
+    """Write content to a file."""
     with open(path, 'w') as f:
         f.write(content)
 
 
 def build_tags_list():
+    """Aggregate all tags from all notes.."""
+
     def read_tags(path):
         lines = open(path, 'r').read().split("\n")
         res = list(filter(lambda x: x.startswith("tags"), lines))
