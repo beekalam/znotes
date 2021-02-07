43bdc0dd6b0c61436817c5bff79e30b12da02453
Mohammad Reza mansouri <beekalam@gmail.com>
2021-02-07 11:41:54 +0330

    [REFACTOR] Cleaned FileStorage addNote further.

--- FileStorage.py
+++ FileStorage.py
@@ -15,11 +15,12 @@ class FileStorage:
         return self.notes
 
     def addNote(self, title: str, content: str, tags: str) -> None:
-        content = "{} {}".format(content, os.linesep)
-        tags = ' '.join(['#' + t.strip() for t in tags.split()])
-        tags = "tags= {} {}".format(tags, os.linesep + os.linesep)
         with open(self._buildFileName(title), 'w') as f:
-            f.writelines([self._buildTitle(title), tags, content])
+            f.writelines([
+                self._buildTitle(title),
+                self._buildTagsLine(tags),
+                self._buildContentLine(content)
+            ])
 
     def _buildFileName(self, title: str) -> str:
         filename = "{} {}.md".format(datetime.now().strftime("%Y%m%d%H%M"), title)
@@ -27,6 +28,13 @@ class FileStorage:
 
     def _buildTitle(self, title: str) -> str:
         return "# {} {}".format(title, os.linesep + os.linesep)
+
+    def _buildContentLine(self, content: str) -> str:
+        return "{} {}".format(content, os.linesep)
+
+    def _buildTagsLine(self, tags: str) -> str:
+        tags = ' '.join(['#' + t.strip() for t in tags.split()])
+        return "tags= {} {}".format(tags, os.linesep + os.linesep)
 
     def removeNote(self, note_id):
         pass
